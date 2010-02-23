592ee0117fae0335727575e80329127cbd04a363
Ryan Voots <simcop2387@simcop2387.info>
2010-02-23 17:04:32 -0500

    adding new karma top plugin

---
+++ plugins/karmatop.pm
@@ -0,0 +1,48 @@
+use POE::Component::IRC::Common qw/l_irc/;
+use DBI;
+use DBD::SQLite;
+
+sub {
+	my( $said, $pm ) = @_;
+	my $body = $said->{body};
+	s/^\s+//, s/\s+$// for $body;
+
+	warn "KARMATOPPLUGIN";
+	use Data::Dumper;
+	warn Dumper $said;
+
+	my $dbh = DBI->connect(
+		"dbi:SQLite:dbname=var/karma.db",
+		"",
+		"",
+		{ RaiseError => 1, PrintError => 0 }
+	);
+
+        if ($said->{body} =~ /\s*(\d+)(\s*karma)?/)
+        {
+           my $count = $1;
+           my $sth;
+           if ($count > 0)
+           {
+             $sth = $dbh->prepare("SELECT subject, kars FROM (SELECT subject, sum(operation) as kars FROM karma GROUP BY subject) AS karmsub ORDER BY kars ASC LIMIT ?");
+           }
+           else
+           {
+             $sth = $dbh->prepare("SELECT subject, kars FROM (SELECT subject, sum(operation) as kars FROM karma GROUP BY subject) AS karmsub ORDER BY kars DESC LIMIT ?");
+           }
+
+           $sth->execute(abs $count);
+
+           while (my $row = $sth->fetchrow_arrayref())
+           {
+              print $row->[0], ": ", $kars, "  ";
+           }
+        }
+        else
+        {
+           print "usage is: top/bottom \d+ karma";
+        }
+}
+
+__DATA__
+karmatop <number>; returns the top or bottom karma for a number of things.
