50def2b8a6250a2569690b8cac0cecbce9f15910
Ryan Voots <simcop2387@simcop2387.info>
2010-10-04 23:34:05 -0400

    adding 8ball

---
+++ plugins/8ball.pm
@@ -0,0 +1,28 @@
+sub {
+	my( $said ) = @_;
+
+    my @a=("Outlook good",
+           "Outlook not so good",
+           "My reply is no",
+           "Don't count on it",
+           "You may rely on it",
+           "Ask again later",
+           "Most likely",
+           "Cannot predict now",
+"Yes","Yes, definitely",
+           "Better not tell you now",
+"It is certain",
+"Very doubtful",
+"It is decidedly so",
+"Concentrate and ask again",
+"Signs point to yes",
+"My sources say no",
+"Without a doubt",
+"Reply hazy, try again",
+"As I see it, yes");
+
+     print $a[rand@a]."."
+}
+
+__DATA__
+8ball, magic 8ball if you don't understand then you need to stop having a life.
