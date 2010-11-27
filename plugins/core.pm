bb97d99d1aa2aaf38bda050c693e1dab56fc7bc6
Ryan Voots <simcop2387@simcop2387.info>
2010-11-27 16:44:47 -0500

    newer core plugin, adds removed

--- plugins/core.pm
+++ plugins/core.pm
@@ -1,12 +1,17 @@
 use Module::CoreList;
 
-no warnings 'void';
 sub {
 	my( $said, $pm ) = @_;
 	my $module = $said->{recommended_args}->[0];
 
 	my $rev = Module::CoreList->first_release( $module );
-	if( $rev ) { print "Added to perl core as of $rev" }
+	if( $rev ) { 
+    print "Added to perl core as of $rev";
+    if ( Module::CoreList->can('removed_from') ) {
+      my $rem = Module::CoreList->removed_from( $module );
+      print " and removed from $rem" if $rem;
+    }
+  }
 	else { 
 
 					my @modules = Module::CoreList->find_modules(qr/$module/); 
@@ -20,7 +25,7 @@ sub {
 					else { 
 							print "Module $module does not appear to be in core. Perhaps capitalization matters or try using the 'cpan' command to search for it." }
 			}
-};
+}
 
 __DATA__
 Tells you when the module you searched for was added to the Perl Core, if it was.
