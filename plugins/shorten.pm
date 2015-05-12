dfadddad9e31178c713a88140864802044d798fd
Ryan Voots <ryan@voots.org>
2015-05-11 20:34:01 -0400

    Switch to tinyurl.com for the shortener since xrl.us is dead

--- plugins/shorten.pm
+++ plugins/shorten.pm
@@ -1,11 +1,14 @@
-use WWW::Shorten 'Metamark';
+
 no warnings 'void';
 sub {
 	my( $said, $pm ) = @_;
 
-	print "New link: ", makeashorterlink($said->{body});
+use WWW::Shorten::TinyURL;
+use WWW::Shorten 'TinyURL';
+
+	print "New link: ", WWW::Shorten::TinyURL::makeashorterlink($said->{body}) // $said->{body};
 }
 
 
 __DATA__
-shorten <url> returns the "short form" of a url. Defaults to using xrl.us.
+shorten <url> returns the "short form" of a url. Defaults to using tinyurl.
