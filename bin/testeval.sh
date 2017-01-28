68d8a5bd4521276aae297c3e615a66860e9586ec
Ryan Voots <ryan@voots.org>
2017-01-27 21:13:56 -0500

    Update the gitignore for the new jail repo and to ignore the small filesystem for it

--- bin/testeval.sh
+++ bin/testeval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -r -d '' CODE <<'EOC'
-perl use IO::Async::Loop; my $loop = IO::Async::Loop->really_new; my $foo; $loop->timeout_future(after => 1.5)->on_done(sub { $foo = 42 })->get; $foo
+perl $SIG{SEGV}="ignore"; $q=unpack "p*", 0xdeadbeef; "DEADBEEF"
 EOC
 
 echo --------
@@ -9,7 +9,7 @@ echo $CODE
 echo --------
 
 rm -f ./jail/noseccomp
-echo $CODE | sudo strace -okilled.log /home/ryan/perl5/perlbrew/perls/perl-blead/bin/perl ./lib/eval.pl
+echo $CODE | sudo strace -ojail/killed.log /home/ryan/perl5/perlbrew/perls/perlbot-inuse/bin/perl5* ./lib/eval.pl
 touch ./jail/noseccomp
-echo $CODE | sudo strace -oalive.log /home/ryan/perl5/perlbrew/perls/perl-blead/bin/perl ./lib/eval.pl
+echo $CODE | sudo strace -ojail/alive.log /home/ryan/perl5/perlbrew/perls/perlbot-inuse/bin/perl5* ./lib/eval.pl
 rm -f ./jail/noseccomp
